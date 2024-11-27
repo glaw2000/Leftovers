58d93fc7865adc0c4b5aebfde1f8e2f1f8f17a3a
Grace Lawrence <grace.h.lawrence@gmail.com>
2024-11-27 17:38:05 +0000

    set debug to False in settings.py

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # if os.path.isfile('env.py'):
 #     import env
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-glaw2000-leftovers-lajp76mi6xk.ws.codeinstitute-ide.net', 'leftovers-app-32aa0915dd0b.herokuapp.com']
 
