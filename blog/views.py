a7b7c1f2221e136b5e5b2006e720c745d25c9013
Grace Lawrence <grace.h.lawrence@gmail.com>
2024-12-02 17:06:21 +0000

    add data query of Post model to AllPosts view to show Title. Change views.py tto Listview for AllPosts

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,13 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+from .models import Post
 
 # Create your views here.
 
-class AllPosts(TemplateView):
+class AllPosts(ListView):
     """
-    Displays posts page
+    Displays listing of all posts
     """
+    model = Post
+    queryset = Post.objects.order_by("created_on")
     template_name = 'blog/blog.html'
+    context_object_name = 'items'
