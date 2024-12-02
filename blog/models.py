5a628f535cbf7ab81e4cd240c9e325b27fc2f7e7
Grace Lawrence <grace.h.lawrence@gmail.com>
2024-12-02 11:48:50 +0000

    add Post model to blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Post(models.Model):
+    """
+    Stores a single blog post entry related to User model by author ID.
+    """
+
+
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    fk_author_id = models.ForeignKey( 
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    content = models.TextField()
+    excerpt = models.TextField(blank=True)
+    image = CloudinaryField('image', default='placeholder')
+    alt_image =models.CharField(max_length=200)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ["-created_on"]
+    def __str__(self):
+        return f"The title of this post is {self.title}"
