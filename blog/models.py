3282754882c984cb2898827de4355ea2a4b5a5d1
Grace Lawrence <grace.h.lawrence@gmail.com>
2024-12-02 12:57:59 +0000

    add Comment model to blog app

--- blog/models.py
+++ blog/models.py
@@ -6,8 +6,6 @@ class Post(models.Model):
     """
     Stores a single blog post entry related to User model by author ID.
     """
-
-
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     fk_author_id = models.ForeignKey( 
@@ -23,3 +21,15 @@ class Post(models.Model):
         ordering = ["-created_on"]
     def __str__(self):
         return f"The title of this post is {self.title}"
+
+class Comment(models.Model):
+    """
+    Stores a single comment related to Post model by Post ID and User model by User Id.
+    """
+    fk_post_id = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    fk_user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    remark = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
