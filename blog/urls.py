11ea00886fb7e9341f2d474793b3389448f7b737
Grace Lawrence <grace.h.lawrence@gmail.com>
2024-12-03 11:23:19 +0000

    add post_detail html and update to urls.py and views.py in blog app for post detail page. Update veg and default imgs to webp

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import AllPosts
 
 urlpatterns = [
-    path('', AllPosts.as_view(), name='blog')
+    path('', AllPosts.as_view(), name='blog'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views.generic import ListView
 from .models import Post
 
@@ -12,3 +13,26 @@ class AllPosts(ListView):
     template_name = 'blog/blog.html'
     context_object_name = 'items'
     paginate_by = 9
+
+def post_detail(request, slug):
+    """
+    Display an individual post :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.order_by("created_on")
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
