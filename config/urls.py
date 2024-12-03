9b9858eb81c4950cf7da30090e411d5ff959094c
Grace Lawrence <grace.h.lawrence@gmail.com>
2024-12-03 12:01:17 +0000

    add formatting to created on text in post_detail.html. Install allauth

--- config/urls.py
+++ config/urls.py
@@ -7,8 +7,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('about/', include('about.urls'), name='about'),
+    path("accounts/", include("allauth.urls")),
+    path('admin/', admin.site.urls),
     path('blog/', include('blog.urls'), name='blog'),
     path('summernote/', include('django_summernote.urls')),
     path('', include('landing.urls'), name='home')
