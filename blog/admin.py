5d766fa3869d876cb721c7ee1ae1d2969525892d
Grace Lawrence <grace.h.lawrence@gmail.com>
2024-12-11 23:34:06 +0000

    add blank lines to admin.py in blog app

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 from .models import Post, Ingredient, Category, Comment, Like
 
+
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
