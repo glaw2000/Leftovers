a64ee4a0e99a89dc1b6e5265bf9afc2c69e20903
Grace Lawrence <grace.h.lawrence@gmail.com>
2024-12-11 21:38:03 +0000

    add doc string to LandingConfig class in apps.py in landing app

--- landing/apps.py
+++ landing/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class LandingConfig(AppConfig):
+    """
+    Provides primary key type for landing app
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'landing'
